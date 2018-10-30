f8f47d169fe0cfd9257d786a84914dffb0a71591
Shalini Aluri <shalinaluri11@gmail.com>
2018-10-29 21:19:07 -0700

    Updated main.java file - output change

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Good Afternoon!");
+        System.out.print("My Second commit to GitHub!");
     }
 }
 //////
